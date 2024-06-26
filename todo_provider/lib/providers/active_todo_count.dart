1b6c458d58f5c8c95273852e90c1010ec395f3f4
yunChul <uiopzx9782@naver.com>
2024-06-26 16:35:26 +0900

    add todo_provider Project

---
+++ todo_provider/lib/providers/active_todo_count.dart
@@ -0,0 +1,39 @@
+import 'package:equatable/equatable.dart';
+import 'package:todo_provider/models/todo_model.dart';
+import 'package:todo_provider/providers/todo_list.dart';
+
+class ActiveTodoCountState extends Equatable {
+  final int activeTodoCount;
+
+  ActiveTodoCountState({required this.activeTodoCount});
+
+  factory ActiveTodoCountState.initial() {
+    return ActiveTodoCountState(activeTodoCount: 0);
+  }
+
+  @override
+  String toString() =>
+      'ActiveTodoCountState(activeTodoCount: $activeTodoCount)';
+
+  ActiveTodoCountState copyWith({
+    int? activeTodoCount,
+  }) {
+    return ActiveTodoCountState(
+      activeTodoCount: activeTodoCount ?? this.activeTodoCount,
+    );
+  }
+
+  List<Object?> get props => [activeTodoCount];
+}
+
+class ActiveTodoCount {
+  final TodoList todoList;
+
+  ActiveTodoCount({required this.todoList});
+
+  ActiveTodoCountState get state => ActiveTodoCountState(
+      activeTodoCount: todoList.state.todos
+          .where((Todo todo) => !todo.completed)
+          .toList()
+          .length);
+}
---
+++ todo_provider/lib/providers/filtered_todos.dart
@@ -0,0 +1,66 @@
+import 'package:equatable/equatable.dart';
+import 'package:todo_provider/models/todo_model.dart';
+import 'package:todo_provider/providers/providers.dart';
+
+class FilteredTodosState extends Equatable {
+  final List<Todo> filteredTodos;
+
+  FilteredTodosState({required this.filteredTodos});
+
+  factory FilteredTodosState.initial() {
+    return FilteredTodosState(filteredTodos: []);
+  }
+
+  @override
+  List<Object?> get props => [filteredTodos];
+
+  FilteredTodosState copyWith({
+    List<Todo>? filteredTodos,
+  }) {
+    return FilteredTodosState(
+      filteredTodos: filteredTodos ?? this.filteredTodos,
+    );
+  }
+
+  @override
+  String toString() => 'FilteredTodosState(filteredTodos: $filteredTodos)';
+}
+
+class FilteredTodos {
+  final TodoFilter todoFilter;
+  final TodoSearch todoSearch;
+  final TodoList todoList;
+
+  FilteredTodos(
+      {required this.todoFilter,
+      required this.todoSearch,
+      required this.todoList});
+
+  FilteredTodosState get state {
+    List<Todo> _filteredTodos;
+
+    switch (todoFilter.state.filter) {
+      case Filter.active:
+        _filteredTodos =
+            todoList.state.todos.where((Todo todo) => !todo.completed).toList();
+        break;
+      case Filter.completed:
+        _filteredTodos =
+            todoList.state.todos.where((Todo todo) => todo.completed).toList();
+        break;
+      case Filter.all:
+      default:
+        _filteredTodos = todoList.state.todos;
+        break;
+    }
+
+    if (todoSearch.state.searchTerm.isNotEmpty) {
+      _filteredTodos = _filteredTodos
+          .where((Todo todo) =>
+              todo.desc.toLowerCase().contains(todoSearch.state.searchTerm))
+          .toList();
+    }
+
+    return FilteredTodosState(filteredTodos: _filteredTodos);
+  }
+}
