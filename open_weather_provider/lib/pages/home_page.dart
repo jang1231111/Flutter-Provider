0d1e277eccc6d19e1855bb6408415d4edac5dd6a
yunChul <uiopzx9782@naver.com>
2024-06-28 16:27:33 +0900

    74 까지

---
+++ open_weather_provider/lib/pages/home_page.dart
@@ -0,0 +1,119 @@
+import 'package:flutter/material.dart';
+import 'package:open_weather_provider/models/weather.dart';
+import 'package:open_weather_provider/pages/search_page.dart';
+import 'package:open_weather_provider/providers/weather/weather_provider.dart';
+import 'package:provider/provider.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  String? _city;
+  late final WeatherProvider _weatherProv;
+
+  @override
+  void initState() {
+    super.initState();
+    _weatherProv = context.read<WeatherProvider>();
+    _weatherProv.addListener(_registerListener);
+  }
+
+  @override
+  void dispose() {
+    _weatherProv.removeListener(_registerListener);
+    super.dispose();
+  }
+
+  void _registerListener() {
+    final WeatherState ws = context.read<WeatherProvider>().state;
+
+    if (ws.status == WeatherStatus.error) {
+      showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            content: Text(ws.error.errMsg),
+          );
+        },
+      );
+    }
+  }
+
+  _fetchWeather() {
+    WidgetsBinding.instance.addPostFrameCallback(
+      (_) async {
+        await context.read<WeatherProvider>().fetchWeather('London');
+      },
+    );
+  }
+
+  Widget _showWeater() {
+    final state = context.watch<WeatherProvider>().state;
+
+    if (state.status == WeatherStatus.initial) {
+      return Center(
+        child: Text(
+          'Select a city',
+          style: const TextStyle(fontSize: 20.0),
+        ),
+      );
+    }
+
+    if (state.status == WeatherStatus.loading) {
+      if (state.status == WeatherStatus.loading) {
+        return const Center(
+          child: CircularProgressIndicator(),
+        );
+      }
+    }
+
+    if (state.status == WeatherStatus.error && state.weather.name == '') {
+      return Center(
+        child: Text(
+          'Select a city',
+          style: const TextStyle(fontSize: 20.0),
+        ),
+      );
+    }
+
+    return Center(
+      child: Text(
+        state.weather.name,
+        style: TextStyle(fontSize: 18.0),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Weather'),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.search_rounded),
+            onPressed: () async {
+              _city = await Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) {
+                    return SearchPage();
+                  },
+                ),
+              );
+              print('city: $_city');
+              if (_city != null) {
+                context.read<WeatherProvider>().fetchWeather(_city!);
+              }
+            },
+          )
+        ],
+      ),
+      body: _showWeater(),
+    );
+  }
+}
---
+++ open_weather_provider/lib/providers/weather/weather_provider.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:open_weather_provider/models/custom_error.dart';
+import 'package:open_weather_provider/models/weather.dart';
+import 'package:open_weather_provider/repositories/weather_repository.dart';
+
+part 'weather_state.dart';
+
+class WeatherProvider with ChangeNotifier {
+  WeatherState _state = WeatherState.initial();
+  WeatherState get state => _state;
+
+  final WeatherRepository weatherRepository;
+  WeatherProvider({required this.weatherRepository});
+
+  Future<void> fetchWeather(String city) async {
+    _state = state.copyWith(status: WeatherStatus.loading);
+    notifyListeners();
+    try {
+      final Weather weather = await weatherRepository.fetchWeather(city);
+
+      _state = state.copyWith(
+        status: WeatherStatus.loaded,
+        weather: weather,
+      );
+      print('state : $_state');
+      notifyListeners();
+    } on CustomError catch (e) {
+      _state = state.copyWith(status: WeatherStatus.error, customError: e);
+      print('state : $_state');
+      notifyListeners();
+    }
+  }
+}
