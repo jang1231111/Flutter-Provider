c376028ccb8c121857866836e158367d76326ea1
yunChul <uiopzx9782@naver.com>
2024-07-01 14:24:36 +0900

    Weather App (Class 78) ChangeNotifierProvider + ChangeNotifierProxyProvider  <= DONE

--- open_weather_provider/lib/pages/home_page.dart
+++ open_weather_provider/lib/pages/home_page.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:open_weather_provider/models/weather.dart';
+import 'package:open_weather_provider/constants/constants.dart';
 import 'package:open_weather_provider/pages/search_page.dart';
-import 'package:open_weather_provider/providers/weather/weather_provider.dart';
+import 'package:open_weather_provider/pages/setting_page.dart';
+import 'package:open_weather_provider/providers/providers.dart';
+import 'package:open_weather_provider/widgets/error_dialog.dart';
 import 'package:provider/provider.dart';
+import 'package:recase/recase.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -32,23 +35,24 @@ class _HomePageState extends State<HomePage> {
     final WeatherState ws = context.read<WeatherProvider>().state;
 
     if (ws.status == WeatherStatus.error) {
-      showDialog(
-        context: context,
-        builder: (context) {
-          return AlertDialog(
-            content: Text(ws.error.errMsg),
-          );
-        },
-      );
+      return errorDialog(context, ws.error.errMsg);
     }
   }
 
-  _fetchWeather() {
-    WidgetsBinding.instance.addPostFrameCallback(
-      (_) async {
-        await context.read<WeatherProvider>().fetchWeather('London');
-      },
-    );
+  // _fetchWeather() {
+  //   WidgetsBinding.instance.addPostFrameCallback(
+  //     (_) async {
+  //       await context.read<WeatherProvider>().fetchWeather('London');
+  //     },
+  //   );
+  // }
+
+  String showTemperature(double temperature) {
+    final tempUnit = context.watch<TempSettingsProvider>().state.tempUnit;
+    if (tempUnit == TempUnit.fahrenheit) {
+      return ((temperature * 9 / 4) + 32).toStringAsFixed(2) + '℉';
+    }
+    return temperature.toStringAsFixed(2) + '℃';
   }
 
   Widget _showWeater() {
@@ -80,11 +84,97 @@ class _HomePageState extends State<HomePage> {
       );
     }
 
-    return Center(
-      child: Text(
-        state.weather.name,
-        style: TextStyle(fontSize: 18.0),
-      ),
+    return ListView(
+      children: [
+        SizedBox(height: MediaQuery.of(context).size.height / 6),
+        Text(
+          state.weather.name,
+          textAlign: TextAlign.center,
+          style: TextStyle(fontSize: 40.0, fontWeight: FontWeight.bold),
+        ),
+        const SizedBox(
+          height: 10.0,
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              TimeOfDay.fromDateTime(state.weather.lastUpdated).format(context),
+              style: const TextStyle(fontSize: 18.0),
+            ),
+            const SizedBox(
+              width: 10.0,
+            ),
+            Text(
+              '(${state.weather.country})',
+              style: const TextStyle(fontSize: 18.0),
+            )
+          ],
+        ),
+        const SizedBox(
+          height: 60.0,
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              showTemperature(state.weather.temp),
+              style: TextStyle(
+                fontSize: 30.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(width: 20.0),
+            Column(
+              children: [
+                Text(
+                  showTemperature(state.weather.tempMax),
+                  style: TextStyle(fontSize: 16.0),
+                ),
+                const SizedBox(
+                  height: 10.0,
+                ),
+                Text(
+                  showTemperature(state.weather.tempMin),
+                  style: TextStyle(fontSize: 16.0),
+                ),
+              ],
+            )
+          ],
+        ),
+        const SizedBox(height: 40.0),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            const Spacer(),
+            showIcon(state.weather.icon),
+            Expanded(
+                flex: 3,
+                child: formaText(
+                  state.weather.description,
+                )),
+            const Spacer(),
+          ],
+        )
+      ],
+    );
+  }
+
+  Widget showIcon(String icon) {
+    return FadeInImage.assetNetwork(
+      placeholder: 'assets/images/loading.gif',
+      image: 'http://$kIconHost/img/wn/$icon@4x.png',
+      width: 96,
+      height: 96,
+    );
+  }
+
+  Widget formaText(String description) {
+    final formattedString = description.titleCase;
+    return Text(
+      formattedString,
+      style: const TextStyle(fontSize: 24.0),
+      textAlign: TextAlign.center,
     );
   }
 
@@ -110,7 +200,17 @@ class _HomePageState extends State<HomePage> {
                 context.read<WeatherProvider>().fetchWeather(_city!);
               }
             },
-          )
+          ),
+          IconButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) {
+                    return SettingsPage();
+                  }),
+                );
+              },
+              icon: Icon(Icons.settings))
         ],
       ),
       body: _showWeater(),
